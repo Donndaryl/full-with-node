edd4262c87b39b68303a666a6b7fc13fa2b20258
DARILLE <donndaryl@gmail.com>
2023-03-12 12:48:26 +0100

    Ajout du model mongoose

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const {json} = require("express");
 const app = express();
+app.use(express.json())
 app.use((req,res,next)=>{
     res.header('Access-Control-Allow-Origin','*');
     res.header('Access-Control-Allow-Headers','Origin, X-Requested-Width,Content,Accept,Content-Type, Authorization');
     res.header('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,PATCH,OPTIONS');
     next();
 })
-app.use('/api/stuff',(req,res)=>{
+app.post('/api/stuff',(req,res,next)=>{
+    console.log(req.body);
+    res.status(200).json({
+        message: 'vos données ont été enregistré !!'
+    });
+})
+app.get('/api/stuff',(req,res)=>{
     const stuff = [
         {
          _id: '1',
---
+++ Backend/models/thing.js
@@ -0,0 +1,10 @@
+const mongodb = require('mongoose')
+const thingScheme = mongodb.Schema({
+    title: {type: String,required: true},
+    description: {type: String,required: true},
+    imageUrl: {type: String,required: true},
+    userId: {type: String,required: true},
+    price: {type: Number,required: true},
+})
+
+module.exports = mongodb.model('thing',thingScheme)
